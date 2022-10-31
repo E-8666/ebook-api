6e3042c369e1b8716ee69d30780f30513b9c2100
E-8666 <fiqriwarior@gmail.com>
2022-10-31 10:44:52 +0700

    modul 3

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+/*
+*task
+*url : http://127.0.0.1:8000/api/halo
+*method: get
+*exec: function
+*return: json => {"me": "ganteng"}
+*/
+
+//Route::get('halo', function(){
+//    $data = ["me" => "guteng"];
+//    return $data;
+//});
+
+Route::resource('helocontroller', HeloController::class);
+
+Route::resource('siswa', SiswaController::class);
+Route::resource('books', BookController::class);
